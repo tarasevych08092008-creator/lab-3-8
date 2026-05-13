5932d431b155a9419c9f8d2050bd34d467ed92e6
tarasevych08092008-hub <tarasevych08092008@gmail.com>
2026-05-13 23:17:16 +0300

    lab4

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'brand', 'created_at', 'updated_at')
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва категорії")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва бренду")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Назва товару")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категорія")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name="Бренд")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
